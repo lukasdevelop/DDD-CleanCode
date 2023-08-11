00977bbef8c6379a2a8808d9f5e283b8cfcf8b02
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-08-11 07:15:47 -0300

    primeiro commit

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,24 @@
+import { Answer } from "../entities/answer"
+import { AnswersRepository } from "../repositories/answers-repository"
+
+interface AnswerQuestionUseCaseRequest {
+    instructorId: string
+    questionId: string
+    content: string
+}
+
+export class AnswerQuestionUseCase {
+    constructor(private answersRepository: AnswersRepository){}
+
+    async execute({ instructorId, questionId, content}: AnswerQuestionUseCaseRequest){
+        const answer = new Answer({
+            authorId: instructorId,
+            content,
+            questionId
+        })
+
+        await this.answersRepository.create(answer)
+
+        return answer
+    }
+}
