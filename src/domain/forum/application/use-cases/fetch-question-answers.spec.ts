8c2c637c4f390a2b8a014188c0ab6dd501db707e
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-08-28 00:55:18 -0300

    feat: listagem das respostas de uma pergunta

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -0,0 +1,58 @@
+import { MakeAnswer } from "test/factories/make-answer";
+import { UniqueEntityID } from "../../enterprise/entities/unique-entity-id";
+import { FetchQuestionAnswersUseCase } from "./fetch-question-answers";
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+
+let inMemoryAnswersRepo: InMemoryAnswersRepository;
+let sut: FetchQuestionAnswersUseCase;
+
+describe("Fetch Question Answers", () => {
+  beforeEach(() => {
+    inMemoryAnswersRepo = new InMemoryAnswersRepository();
+    sut = new FetchQuestionAnswersUseCase(inMemoryAnswersRepo);
+  });
+
+  it("should be able to fetch question answers", async () => {
+    await inMemoryAnswersRepo.create(
+      MakeAnswer({
+        questionId: new UniqueEntityID("question-1"),
+      }),
+    );
+
+    await inMemoryAnswersRepo.create(
+      MakeAnswer({
+        questionId: new UniqueEntityID("question-1"),
+      }),
+    );
+
+    await inMemoryAnswersRepo.create(
+      MakeAnswer({
+        questionId: new UniqueEntityID("question-1"),
+      }),
+    );
+
+    const { answers } = await sut.execute({
+      questionId: "question-1",
+      page: 1,
+    });
+
+    expect(answers).toHaveLength(3);
+  });
+});
+
+it("should be able to fetch paginated question answers", async () => {
+  for (let i = 1; i <= 22; i++) {
+    await inMemoryAnswersRepo.create(
+      MakeAnswer({
+        questionId: new UniqueEntityID("question-1"),
+      }),
+    );
+  }
+
+  const { answers } = await sut.execute({
+    questionId: "question-1",
+    page: 2,
+  });
+
+  expect(answers).toHaveLength(5);
+});
