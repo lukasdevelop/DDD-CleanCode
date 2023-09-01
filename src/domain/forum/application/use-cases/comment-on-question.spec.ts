f691f8aceb2a2046773da093820f4fd5226ec52d
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-09-01 00:27:22 -0300

    refac: teste error

--- src/domain/forum/application/use-cases/comment-on-question.spec.ts
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryQuestionCommentsRepository } from "test/repositories/in-memory-question-comments-repository";
 import { DeleteQuestionCommentUseCase } from "./delete-question-comment";
 import { MakeQuestionCommentComment } from "test/factories/make-question-comment";
+import { UniqueEntityID } from "../../enterprise/entities/unique-entity-id";
 
 let inMemoryQuestionCommentsRepo: InMemoryQuestionCommentsRepository;
 let sut: DeleteQuestionCommentUseCase;
@@ -22,5 +23,20 @@ describe("Delete question commment", () => {
     });
 
     expect(inMemoryQuestionCommentsRepo.items).toHaveLength(0);
+  });
+
+  it("should not be able to delete another user question comment", async () => {
+    const questionComment = MakeQuestionCommentComment({
+      authorId: new UniqueEntityID("author-1"),
+    });
+
+    await inMemoryQuestionCommentsRepo.create(questionComment);
+
+    expect(() => {
+      return sut.execute({
+        questionCommentId: questionComment.id.toString(),
+        authorId: "author-2",
+      });
+    }).rejects.toBeInstanceOf(Error);
   });
 });
